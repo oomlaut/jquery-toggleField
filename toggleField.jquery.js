a503620ccdeae43055165cc5830bb025e87ba834
Paul Gueller <pgueller@laughlin.com>
2011-07-23 20:49:15 -0500

    Clean up documentation
    Move 'usage' to README, update description to reflect new functionality

--- toggleField.jquery.js
+++ toggleField.jquery.js
@@ -1,24 +1,15 @@
 /**
  * Toggle the values of a text-based input field
  *
- * jQuery plugin that references the accessible TITLE attribute of an 
- * INPUT element to populate the VALUE of the field until the focus/blur
- * event is triggered. Useful as an indicator of expected values or in
- * addition to LABELs
+ * jQuery plugin that enables the accessible PLACEHOLDER attribute of an 
+ * INPUT element. If unsupported, populate the VALUE of the field until
+ * the focus/blur event is triggered. Useful as an indicator of expected
+ * values or in addition to LABELs
  *
  * @author Paul Gueller http://paulgueller.com
  * @version 2.2.1
  * @since 2011.0722
  * @requires jQuery v1.3+
- *
- * @usage: jQuery('input').togglefield();
- *	applies behavior to all input elements
- *
- * @usage: jQuery('[type="url"]').togglefield({textsrc:"title"});
- *	references "title" attribute for placeholder text. Useful for accessibility
- *
- * @usage: jQuery('input:password').togglefield({textsrc:"label"});
- *	uses the label text for placeholder. Handy when labels are hidden.
  */
  
 ;
@@ -67,7 +58,6 @@ $.fn.toggleField = function (options) {
 		var $this = $(this);
 		
 		if(!$this.prop('placeholder')) {
-			//set a placeholder value if one is not already set
 			switch(context.settings.textsrc){
 				case "label":
 					var txt = $('label[for="'+$this.attr('id')+'"]').text();
@@ -82,9 +72,7 @@ $.fn.toggleField = function (options) {
 		}
 		
 		if(!context.supports_input_placeholder()) {
-			//provide placeholder-esque support for browsers that to not
 			if($this.attr('type') === "password") {
-				//special password helper due to security issues
 				context.pwHelper($this);
 			} else {
 				context.toggle($this);
